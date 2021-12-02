e5ea005386e4a6f82dd24990d151d260153daceb
Imtiaz Ahmed <50087271+ImtiazVision@users.noreply.github.com>
2021-12-02 13:08:36 -0500

    Update fibonacci.py

--- fibonacci.py
+++ fibonacci.py
@@ -14,9 +14,9 @@ def fib(n):
 
     count = 3  # Starting from 3 because we already know the first two values
     while count <= n:
-        fib_n = first + second
-        first = second
-        second = fib_n
+        fib_n = first + second # we are adding first and 2nd 
+        first = second # here we are setting 2nd to first
+        second = fib_n # here we are appointing addition of 1st and 2nd into 2nd
         count += 1  # Increment count in each iteration
     return fib_n
 
