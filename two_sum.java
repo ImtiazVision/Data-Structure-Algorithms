4364ef8504139543149a3b973060696b1b58501f
Imtiaz Ahmed <ahmedimtiaz.ahmed@gmail.com>
2021-12-02 13:30:20 -0500

    two_sum

---
+++ two_sum.java
@@ -0,0 +1,18 @@
+// Return the indices of the two numbers that add up to target given an array nums and an integer target.
+
+class Solution {
+  public int[] twoSum(int[] nums, int target) {
+    Map<Integer, Integer> map = new HashMap<>();
+    for (int i = 0; i < nums.length; i++) {
+      map.put(nums[i], i);
+    }
+    for (int i = 0; i < nums.length; i++) {
+      int complement = target - nums[i];
+      if (map.containsKey(complement) && map.get(complement) != i) {
+        return new int[] { i, map.get(complement) };
+      }
+    }
+    // In case there is no solution, null is returned.
+    return null;
+  }
+}
