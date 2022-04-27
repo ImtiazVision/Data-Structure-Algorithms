21ab7e7b9622dfe17031142a6e7a5cc4a41616e2
Imtiaz Ahmed <50087271+ImtiazVision@users.noreply.github.com>
2022-04-27 02:55:30 -0400

    Update binary_search.java

--- binary_search.java
+++ binary_search.java
@@ -1,1 +1,26 @@
 // Given an array of integers nums which is sorted in ascending order, and an integer target, write a function to search target in nums. If target exists, then return its index. Otherwise, return -1.
+/* Binary search is a basic method that compares the target value to the array's middle member.
+
+If the goal value equals the center element, we're done.
+
+
+
+If the goal value is lower, continue to search on the left.
+
+
+
+If the goal value is higher, continue searching on the right. */
+
+
+class Solution {
+  public int search(int[] nums, int target) {
+    int pivot, left = 0, right = nums.length - 1;
+    while (left <= right) {
+      pivot = left + (right - left) / 2;
+      if (nums[pivot] == target) return pivot;
+      if (target < nums[pivot]) right = pivot - 1;
+      else left = pivot + 1;
+    }
+    return -1;
+  }
+}
